6cef141674417e12a07e8a34d08cda62e825407c
Bret <horfius@gmail.com>
2013-11-16 20:34:33 -0500

    Revert "Initial Mod info"
    This reverts commit 8420ca4026cd1c207ab090837ebcb6970951beff.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 iChunMod
 ========
-Because why not?
--- ichunmod/lib/Reference.java
+++
@@ -1,9 +0,0 @@
-package ichunmod.lib;
-
-public class Reference {
-	
-	public static final String MOD_ID = "ichunmod"; 
-	public static final String MOD_NAME = "iChunMod";
-	public static final String VERSION = "a0.0.1"; //Alpha 0.0.1
-	public static final String CHANNELS = MOD_ID;
-}
